67424a10f7878111ad7d0848ac0c3266bf5cdd49
Susheel Mannepalli <susheel.mannepalli@gmail.com>
2016-12-18 16:06:15 -0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,7 @@ socket.on('newMessage', function (message) {
   li.text(`${message.from}: ${message.text}`);
   jQuery('#messages').append(li);
 });
+
 socket.on('newLocationMessage', function(message) {
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>')
@@ -21,27 +22,33 @@ socket.on('newLocationMessage', function(message) {
   li.append(a);
   jQuery('#messages').append(li);
 });
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('')
   });
 });
 
 var locationButton = jQuery('#send-location');
+
 locationButton.on('click', function () {
   if(!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
     alert('Unable to fetch your location');
+    locationButton.removeAttr('disabled').text('Send location');
   });
 });
